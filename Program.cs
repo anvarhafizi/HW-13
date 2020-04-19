98220e1e89f33153865af757bd1bca755e28e746
unknown <yourGitHub@example.com>
2020-04-19 19:02:10 +0500

    the finnaly code!

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace HW_13
     {
         static void Main()
         {
-
+             //Using the SLICE operator;
+            ArrayHelper<string> List = new ArrayHelper<string>();
+            string[]    K= {"Hello","World","and","Republic","of","Tajikistan"};
+            List.Slice(ref K, 1, -3);
+            foreach(var i in K)
+            {
+                System.Console.Write(i+",");
+            }
         }
     }
     class ArrayHelper<M>
@@ -53,6 +60,65 @@ namespace HW_13
         }
         arr = a;
     }
-
+    public M[] Slice(ref M[] arr, int x)
+    {
+        if (x < 0)
+        {
+            M[] arr1 = new M[Math.Abs(x)];
+            int b = 0;
+            bool bul = true;
+            for (int i = arr.Length - 1; bul; i--)
+            {
+                bul = !(b == Math.Abs(x) - 1);
+                arr1[b] = arr[i];
+                b++;
+            }
+            return arr1;
+        }
+        else if (x > 0)
+        {
+            M[] arr1 = new M[arr.Length - x + 1];
+            int b = 0;
+            for (int i = 0; i <= x; i++)
+            {
+                arr1[b] = arr[i];
+                b++;
+            }
+            return arr1;
+        }
+        else
+        {
+            return arr;
+        }
+    }
+    public M[] Slice(ref M[] arr, int x, int y)
+    {
+        if (x > 0 && y > 0)
+        {
+            M[] arr1 = new M[y - x + 1];
+            int c = 0;
+            for (int i = x; i <= y; i++)
+            {
+                arr1[c] = arr[i];
+                c++;
+            }
+            return arr1;
+        }
+        else if (x > 0 && y < 0)
+        {
+            M[] arr1 = new M[arr.Length - Math.Abs(y) - 1];
+            int b = 0;
+            for (int i = x; i <= arr1.Length; i++)
+            {
+                arr1[b] = arr[i];
+                b++;
+            }
+            return arr1;
+        }
+        else
+        {
+            return arr;
+        }
+    }
     }
 }
